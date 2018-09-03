0d4bdddb136d170b882a9f53a49eadf7490ad6c1
devil <hongz1125@qq.com>
2018-09-03 16:24:47 +0800

    hz up

---
+++ wx_001/db/index.js
@@ -0,0 +1,17 @@
+let mysql_cont = {   
+     host: '127.0.0.1',     
+     user: 'root',   
+     password: 'hongz1126',  
+     database:'mini', // 前面建的user表位于这个数据库中 
+      port: 3306  
+};
+
+
+
+
+
+
+
+
+
+
